968fe9be229f94c6346ea1db4a1623daea129d6b
Muhammad Haseeb <muhmmedhaseeb2003@gmail.com>
2026-02-04 18:02:45 +0500

    wrote helper methods and done with the user authentication.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,25 @@
 class SessionsController < ApplicationController
   def new
   end
+  def create  
+    user = User.find_by(username: params[:session][:username])
+    check = user && user.authenticate(params[:session][:password])
+    if check
+      session[:user_id] = user.id
+      flash[:success] = "#{user.username.capitalize} have successfully logged in."  
+      redirect_to root_path
+    else
+      flash.now[:error] = "Credentials were incorrect."
+      render 'new'
+    end 
+  end
+
+  def destroy
+    
+    session[:user_id] = nil
+    flash[:success] = "You have logged out successfully"
+    redirect_to login_path
+
+  end
+
 end
